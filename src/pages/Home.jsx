65087b3b5b34793c7828b99f02dea60561dad57d
adjihyphen <adji.hendrawan@moneyherogroup.com>
2025-07-31 20:18:39 +0700

    Getting Content from NY API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,14 @@ import { callArticles } from "../services/call";
 import ArticleSection from "../components/ArticleSection";
 
 export default function Home() {
+
+  const [articles, setArticles] = useState([]);
+  const API_SECTION = 'home';
+
+  useEffect(() => {
+    callArticles(API_SECTION).then(setArticles);
+  }, []);
+
   return (
     <>
       <header>
@@ -38,37 +46,11 @@ export default function Home() {
           <a href="#">See All</a>
         </div>
 
-        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-10">
-          {[...Array(6)].map((_, index) => (
-            <div key={index} className="flex flex-col justify-between border-b pb-5">
-              <a href="#" className="text-gray-900 font-medium hover:text-indigo-600 text-left">
-                {index % 2 === 0
-                  ? "Lorem ipsum dolor sir amet"
-                  : "Lorem ipsum dolor sir amet achtung"}
-              </a>
-              <div className="flex items-start justify-between mt-3">
-                <div className="text-sm w-2/3 text-left">
-                  <p className="text-gray-700 text-left">
-                    Lorem ipsum dolor sir amet amet jabang bayi with the lorem lorem...
-                  </p>
-                  <p className="text-gray-600 text-xs">Aug 18</p>
-                </div>
-                <a href="#" className="inline-block ml-2">
-                  <div
-                    className="w-20 h-20 bg-cover bg-center"
-                    style={{
-                      backgroundImage: `url(${
-                        index % 2 === 0
-                          ? "https://placehold.co/400"
-                          : "https://placehold.co/400"
-                      })`,
-                    }}
-                  />
-                </a>
-              </div>
-            </div>
+          <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-7">
+         {articles.map((a) => (
+            <ArticleSection key={a.url} article={a} />
           ))}
-        </div>
+          </div>
       </div>
     </>
     
