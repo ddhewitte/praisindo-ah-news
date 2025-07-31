2d7b22c7eb14ab197b72b94458681a220f852f3d
adjihyphen <adji.hendrawan@moneyherogroup.com>
2025-07-31 23:07:47 +0700

    Showing search in result html

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -11,39 +11,36 @@ export default function Search() {
 
     const query = searchParams.get("q");
 
-    console.log(query);
-
     useEffect(() => {
         if (query) {
-        searchArticles(query).then(setQueryResults)
+            searchArticles(query).then(setQueryResults)
         }
-
-    console.log(queryResult)
-        
     }, [query]);
 
     return (
         <>
-        <Header />
-        <Navbar />
+            <Header />
+            <Navbar />
 
-        <div className="mt-6"><h3>Hasil pencarian : </h3></div>
+            <div className="mt-6"><h3>Hasil pencarian : </h3></div>
 
-        <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:py-8 md:px-16 relative">
-            <div className="flex flex-col justify-between border-b pb-5">
-                <a href="https://www.nytimes.com/..." className="text-gray-900 font-medium hover:text-indigo-600 text-left">
-                    Title Dolor sir Amter
-                </a>
-                <div className="flex items-start justify-between mt-3">
-                    <div className="text-sm w-2/3 text-left">
-                        <p className="text-gray-600 text-xs mt-2 font-bold">2025-07-31</p>
-                        <p className="text-gray-700 text-left">
-                            DEsc lorem ipsum dolor sir amet amet jabang bayi
-                        </p>
+            <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:py-8 md:px-16 relative">
+                {queryResult.map((article) => (
+                    <div key={article._id} className="flex flex-col justify-between border-b pb-5 mb-5">
+                        <a href={article.web_url} className="text-gray-900 font-medium hover:text-indigo-600 text-left">
+                            {article.headline.main}
+                        </a>
+                        <div className="flex items-start justify-between">
+                            <div className="text-sm w-2/3 text-left">
+                                <p className="text-gray-600 text-xs mt-2 font-bold">2025-07-31</p>
+                                <p className="text-gray-700 text-left">
+                                    {article.abstract}
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                ))}    
             </div>
-        </div>
         </>
     );
 }
